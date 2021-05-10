819de6efb582157cfdf89ee12d9f11a560d7ea4a
dvidals <despedidachicho@hotmail.com>
2021-05-10 12:32:22 +0200

    lunes 10 maio

--- MiniCalculadoraApp/src/app/app.component.ts
+++ MiniCalculadoraApp/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MiniCalculadoraApp';
+  title = 'Mini Calculadora';
+  n1:string="0";
+  n2:string="0";
+  resultado:number=0;
+  operacion:string="";
+  sumar():void{
+    this.resultado=parseInt(this.n1)+ parseInt(this.n2);
+  }
+  restar():void{
+    this.resultado=parseInt(this.n1)- parseInt(this.n2);
+  }
+  multiplicar():void{
+    this.resultado=parseInt(this.n1)* parseInt(this.n2);
+  }
+  dividir():void{
+    this.resultado=parseInt(this.n1)/ parseInt(this.n2);
+  }
 }
